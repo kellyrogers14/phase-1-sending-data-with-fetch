da9d961a87acf6494f3b35f0da672e1b15047814
Kelly Rogers <kellyerogers92@gmail.com>
2024-01-26 19:32:12 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+
+function submitData(name, email) {
+    
+    const newUser = {
+        name: name,
+        email: email
+    }
+
+    const configureUser = {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(newUser)
+    }
+
+    return fetch ("http://localhost:3000/users", configureUser)
+    .then (response => response.json())
+    .then (data => document.body.innerHTML = data.id)
+    .catch(error => document.body.innerHTML = error.message)
+}
